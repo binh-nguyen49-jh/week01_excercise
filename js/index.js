f3b59d1e80f8f6bf8e93871646d8dfad133f417f
huubinh49 <hocchui0409@gmail.com>
2022-03-31 11:26:01 +0700

    fix section ends

--- js/index.js
+++ js/index.js
@@ -1,23 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const brightassSection = document.getElementById("brightass");
-  const brightassContainer = document.querySelector("#brightass .container");
-
   const wordsSection = document.getElementById("words");
   const wordsImage = document.querySelector("#words .section__img img");
   const wordsContainer = document.querySelector("#words .container");
 
-  const endSection = document.getElementById("end");
-  const endContainer = document.querySelector("#end .container");
-
-  const resizeHeightBrightass = () => {
-    if (window.innerWidth > 1599) {
-      brightassSection.style.height =
-        `${0.5 * brightassContainer.getBoundingClientRect().height}px`;
-    } else {
-      brightassSection.style.height =
-        `${brightassContainer.getBoundingClientRect().height}px`;
-    }
-  }
   const resizeHeightWords = () => {
     const wordsContainerHeight = wordsContainer.getBoundingClientRect().height;
     const wordsImageHeight = wordsImage.getBoundingClientRect().height;
@@ -32,23 +17,10 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   }
 
-  const resizeHeightEnd = () => {
-    if (window.innerWidth > 1599) {
-      endSection.style.height =
-        `${0.5 * endContainer.getBoundingClientRect().height + 120}px`;
-    } else {
-      endSection.style.height =
-        `${endContainer.getBoundingClientRect().height}px`;
-    }
-  }
-  // resizeHeightBrightass();
   resizeHeightWords();
-  resizeHeightEnd();
 
   window.onresize = () => {
-    // resizeHeightBrightass();
     resizeHeightWords();
-    resizeHeightEnd();
   }
 
   const header = document.querySelector("header");
